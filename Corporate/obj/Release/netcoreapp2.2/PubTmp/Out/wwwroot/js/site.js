8c5657c955f54beff0a7db8e25bbb26eaf44aa6f
Mike Wright <mike.wright@itsallwater.net>
2018-12-27 14:44:45 -0700

    Clean up, clean up

--- Corporate/obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/site.js
+++ Corporate/obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/site.js
@@ -4,15 +4,15 @@
 // Load
 $(function () {
     $('#home img').data('ow', $('#home img').width());
-})
+});
 
 // Go Home
 $('#home').on('click', function (e) {
     $('.active').removeClass('active');
     $("#home img").animate({ width: $('#home img').data('ow') }, 100);
-})
+});
 
 // Resize Logo
 $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
     $("#home img").animate({ width: '20%' }, 100);
-})
+});
