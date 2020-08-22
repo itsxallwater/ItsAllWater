e68d25df99282e378cd1ce1098dbc3742b50d2a0
Mike Wright <mike.wright@itsallwater.net>
2020-08-22 11:40:11 -0700

    Portfolio page

---
+++ src/components/ProfileItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <a :href="item.url" target="_blank" rel="noopener" :alt="item.alt">
+    <i :class="item.icon" /><span class="md:hidden pl-2">{{ item.alt }}</span>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      url: String,
+      icon: String,
+      alt: String,
+    },
+  },
+  setup(props) {},
+};
+</script>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -2,7 +2,7 @@
   <div>
     <!--Hero-->
     <div
-      class="container px-3 mb-16 mx-auto flex flex-wrap flex-col md:flex-row items-center"
+      class="container px-3 mb-8 md:mb-4 mx-auto flex flex-wrap flex-col md:flex-row items-center"
     >
       <!--Left Col-->
       <div class="w-full md:w-3/5 items-center">
@@ -17,77 +17,17 @@
         >
           My Work
         </h1>
-        <ol class="leading-normal text-2xl mb-6">
-          <li>
-            <a href="./resume.pdf" target="_blank" rel="noopener" alt="Resume"
-              >Resume</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://www.donovanvsdevin.com"
-              alt="Donovan vs. Devin"
-              target="_blank"
-              rel="noopener"
-              >Donovan vs. Devin</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/michaelscottwright/"
-              target="_blank"
-              rel="noopener"
-              alt="LinkedIn michaelscottwright"
-              ><i class="fab fa-linkedin-in pr-2"></i>michaelscottwright</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://github.com/itsxallwater"
-              target="_blank"
-              rel="noopener"
-              alt="GitHub itsxallwater"
-              ><i class="fab fa-github-alt pr-2"></i>itsxallwater</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://stackoverflow.com/story/mikewright"
-              target="_blank"
-              rel="noopener"
-              alt="StackOverflow mikewright"
-              ><i class="fab fa-stack-overflow pr-2"></i>mikewright</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://www.hackerrank.com/mike_wright"
-              target="_blank"
-              rel="noopener"
-              alt="HackerRank mike_wright"
-              ><i class="fab fa-hackerrank pr-2"></i>mike_wright</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://angel.co/mike-wright-15"
-              target="_blank"
-              rel="noopener"
-              alt="Angel.co mike-wright-15"
-              ><i class="fab fa-angellist pr-2"></i>mike-wright-15</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://app.pluralsight.com/profile/mike-wright-dc"
-              target="_blank"
-              rel="noopener"
-              alt="PluralSight mike-wright-dc"
-            >
-              PluralSight</a
-            >
-          </li>
-        </ol>
+        <div class="flex flex-wrap mb-4 leading-normal text-xl md:text-3xl">
+          <div class="w-full md:w-1/3 md:h-12" v-for="item in items">
+            <ProfileItem :item="item" />
+          </div>
+        </div>
+        <span class="md:text-sm italic"
+          >Icons thanks to
+          <a href="https://icones.netlify.app/" rel="noopener" target="_blank"
+            >Icones</a
+          >!</span
+        >
       </div>
     </div>
     <TransitionTop />
@@ -96,11 +36,60 @@
 
 <script>
 import TransitionTop from "../components/TransitionTop.vue";
+import ProfileItem from "../components/ProfileItem.vue";
 
 export default {
   name: "Portfolio",
   components: {
-    TransitionTop
-  }
+    ProfileItem,
+    TransitionTop,
+  },
+  data() {
+    return {
+      items: [
+        { url: "./resume.pdf", icon: "i icomoon-free:profile", alt: "Resume" },
+        {
+          url: "https://www.donovanvsdevin.com",
+          icon: "i mdi:basketball-hoop-outline",
+          alt: "Donovan vs. Devin",
+        },
+        {
+          url: "https://www.linkedin.com/in/michaelscottwright/",
+          icon: "i simple-icons:linkedin",
+          alt: "LinkedIn",
+        },
+        {
+          url: "https://github.com/itsxallwater",
+          icon: "i simple-icons:github",
+          alt: "GitHub",
+        },
+        {
+          url: "https://stackoverflow.com/story/mikewright",
+          icon: "i simple-icons:stackoverflow",
+          alt: "Stack Overflow",
+        },
+        {
+          url: "https://www.hackerrank.com/mike_wright",
+          icon: "i simple-icons:hackerrank",
+          alt: "Hacker Rank",
+        },
+        {
+          url: "https://angel.co/mike-wright-15",
+          icon: "i simple-icons:angellist",
+          alt: "Angel List",
+        },
+        {
+          url: "https://app.pluralsight.com/profile/mike-wright-dc",
+          icon: "i simple-icons:pluralsight",
+          alt: "Pluralsight",
+        },
+        {
+          url: "https://hub.docker.com/u/itsxallwater",
+          icon: "i simple-icons:docker",
+          alt: "Docker",
+        },
+      ],
+    };
+  },
 };
 </script>
